f98d8a07d8931ee15c40947cfd2a2106db54fdcc
Raphael Lira <12634847785@cefet-rj.br>
2025-01-21 17:28:33 -0300

    trabalho 1

--- trabalho.sh
+++ trabalho.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
 
+cd
+
+mkdir joao maria
+
+cd ~/joao
+
+cp -r /etc/systemd ~/joao
+
+cd ~/maria
+
+cp /etc/passwd ~/maria
+
+cp /etc/group ~/maria
+
+cp -r ~/joao/. ~/joao_copia
+
+cp -r ~/maria/. ~/maria_copia
+
+cd
+
+mkdir mais_uma_pasta
+
+cp -r ~/joao ~/mais_uma_pasta
+
+cp -r ~/maria ~/mais_uma_pasta
+
+mv ~/mais_uma_pasta/joao ~/mais_uma_pasta/joao_temp
+
+mv ~/mais_uma_pasta/maria ~/mais_uma_pasta/joao
+
+mv ~/mais_uma_pasta/joao_temp ~/mais_uma_pasta/maria
+
 # NAO APAGUE A LINHA ACIMA!
 
 
